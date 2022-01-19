8064dd6d8de8d060065cc77272e4fb39815ce817
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-01-19 02:45:00 -0400

    Initial commit

---
+++ src/main/java/com/example/springpractices/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springpractices;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringPracticesApplication.class);
+    }
+
+}
---
+++ src/test/java/com/example/springpractices/SpringPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
